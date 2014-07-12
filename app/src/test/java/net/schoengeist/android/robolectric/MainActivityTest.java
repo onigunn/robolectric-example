dea3911973d6a7fb025a3484060f6f2849ec9711
Onur Guengoeren <guengoeren@schoengeist.net>
2014-07-12 12:18:35 +0200

    added junit test

--- app/src/test/java/net/schoengeist/android/robolectric/MainActivityTest.java
+++ app/src/test/java/net/schoengeist/android/robolectric/MainActivityTest.java
@@ -1,14 +1,19 @@
 package net.schoengeist.android.robolectric;
 
 import android.app.Activity;
+import android.view.View;
+import android.widget.TextView;
 
 import junit.framework.Assert;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
+import static org.fest.assertions.api.ANDROID.assertThat;
 import static org.junit.Assert.assertTrue;
 
 
@@ -16,9 +21,28 @@ import static org.junit.Assert.assertTrue;
 @RunWith(RobolectricTestRunner.class)
 public class MainActivityTest {
 
-    @Test
-    public void testSomething() throws Exception {
-        Activity activity = Robolectric.buildActivity(MainActivity.class).create().get();
-        assertTrue(activity != null);
+    private Activity sut;
+
+    @Before
+    public void setup() {
+        sut = Robolectric.buildActivity(MainActivity.class).create().get();
     }
+
+    @Test
+    public void shouldBeNotNull() throws Exception {
+        assertTrue(sut != null);
+    }
+
+    @Test
+    public void shouldHoldTextView() throws Exception {
+        View textView = sut.findViewById(R.id.textView);
+        assertThat(textView).isNotNull();
+    }
+
+    @Test
+    public void shouldDisplayHelloWorld() throws Exception {
+        TextView textView = (TextView) sut.findViewById(R.id.textView);
+        assertThat(textView).containsText(R.string.hello_world);
+    }
+
 }
