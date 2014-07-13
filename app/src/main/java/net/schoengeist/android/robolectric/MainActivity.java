675f61e3b527b6a6bde01e4a8a51dbc1021abb1d
Onur Guengoeren <guengoeren@schoengeist.net>
2014-07-13 12:20:03 +0200

    added tests

--- app/src/main/java/net/schoengeist/android/robolectric/MainActivity.java
+++ app/src/main/java/net/schoengeist/android/robolectric/MainActivity.java
@@ -1,17 +1,38 @@
 package net.schoengeist.android.robolectric;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends Activity {
+
+    private Button mButton2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mButton2 = (Button) findViewById(R.id.button2);
+        findViewById(R.id.button1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mButton2.setVisibility(View.INVISIBLE);
+            }
+        });
+
+
+        mButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, MainActivity.class));
+            }
+        });
     }
 
 
--- app/src/test/java/net/schoengeist/android/robolectric/MainActivityTest.java
+++ app/src/test/java/net/schoengeist/android/robolectric/MainActivityTest.java
@@ -1,7 +1,9 @@
 package net.schoengeist.android.robolectric;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import junit.framework.Assert;
@@ -15,6 +17,7 @@ import org.robolectric.annotation.Config;
 
 import static org.fest.assertions.api.ANDROID.assertThat;
 import static org.junit.Assert.assertTrue;
+import static org.robolectric.Robolectric.shadowOf;
 
 
 @Config(manifest = "./src/main/AndroidManifest.xml")
@@ -45,4 +48,29 @@ public class MainActivityTest {
         assertThat(textView).containsText(R.string.hello_world);
     }
 
+    @Test
+    public void shouldHaveThreeButtons() throws Exception {
+        LinearLayout layout = (LinearLayout) sut.findViewById(R.id.linearLayout);
+        assertThat(layout)
+                .hasChildCount(3)
+                .isVisible();
+    }
+
+    @Test
+    public void shouldHideButton2OnButton1Click() throws Exception {
+        sut.findViewById(R.id.button1).performClick();
+
+        View button2 = sut.findViewById(R.id.button2);
+        assertThat(button2).isInvisible();
+    }
+
+    @Test
+    public void shouldStartIntent() throws Exception {
+        Intent expectedIntent = new Intent(sut, MainActivity.class);
+
+        sut.findViewById(R.id.button2).performClick();
+        Intent startedActivity = shadowOf(sut).getNextStartedActivity();
+
+        assertThat(startedActivity).isEqualTo(expectedIntent);
+    }
 }
